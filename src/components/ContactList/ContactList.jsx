00111c1226fc9bf580b951b2835944eed275aea4
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2022-12-13 17:15:17 +0200

    bugfix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { Contact } from '../ContactItem/ContactItem';
 
 export const ContactList = ({ contacts, onDelete }) => {
--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 export const Notification = ({ filter }) => {
   return <p>{filter} not found</p>;
